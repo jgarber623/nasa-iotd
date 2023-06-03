a07087f9887c768a09bceaac67c8439cb7f17534
Jason Garber <jason@sixtwothree.org>
2023-06-02 21:57:28 -0400

    📦 Cargo cult a bunch of config from nasa-apod

---
+++ .eleventy.js
@@ -0,0 +1,32 @@
+const { EleventyHtmlBasePlugin, EleventyRenderPlugin } = require('@11ty/eleventy');
+
+module.exports = function(eleventyConfig) {
+  // Global Data
+  eleventyConfig.addGlobalData('app', require('./src/manifest.webmanifest.json'));
+
+  // Passthrough File Copy
+  eleventyConfig
+    .addPassthroughCopy('./src/assets')
+    .addPassthroughCopy('./src/favicon.ico')
+    .addPassthroughCopy({
+      './src/manifest.webmanifest.json': 'manifest.webmanifest'
+    });
+
+  // Libraries
+  eleventyConfig.setLibrary('md', require('./lib/libraries/markdown.js'));
+  eleventyConfig.setLiquidOptions(require('./lib/libraries/liquid.js'));
+
+  // Filters
+  eleventyConfig.addFilter('markdown', require('./lib/filters/markdown.js'));
+
+  // Plugins
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+  eleventyConfig.addPlugin(EleventyRenderPlugin);
+
+  return {
+    dir: {
+      input: './src'
+    },
+    pathPrefix: '/nasa-iotd/'
+  };
+};
