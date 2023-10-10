35801e2dffe55f62a45dd0bfe7f9536dd9fc9081
Jason Garber <jason@sixtwothree.org>
2023-10-10 13:46:58 -0400

    Streamline ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,1 @@
-const globals = require('globals');
-
-const config = require('@jgarber/eslint-config');
-
-module.exports = [
-  ...config,
-  {
-    files: ['**/*.js'],
-    languageOptions: {
-      globals: {
-        ...globals.nodeBuiltin
-      }
-    }
-  }
-];
+module.exports = require('@jgarber/eslint-config');
