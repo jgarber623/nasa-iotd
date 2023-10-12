62fb0645b6bf32b67f135763e11794199ca5148d
Jason Garber <jason@sixtwothree.org>
2023-10-11 22:35:34 -0400

    Configure fetch global

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,13 @@
-module.exports = require('@jgarber/eslint-config');
+const config = require('@jgarber/eslint-config');
+
+module.exports = [
+  ...config,
+  {
+    files: ['lib/**/*.js'],
+    languageOptions: {
+      globals: {
+        fetch: 'readonly'
+      }
+    }
+  }
+];
