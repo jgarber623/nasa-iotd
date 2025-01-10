95812185c27e125cdfd982646d1d9884f6d2a348
Jason Garber <jason@sixtwothree.org>
2025-01-09 22:11:11 -0500

    Simplify ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,1 @@
-import config from "@jgarber/eslint-config";
-
-export default [
-  ...config,
-  {
-    files: ["lib/**/*.js"],
-    languageOptions: {
-      globals: {
-        fetch: "readonly",
-      },
-    },
-  },
-];
+export { default } from "@jgarber/eslint-config";
